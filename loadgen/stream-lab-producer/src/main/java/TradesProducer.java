f6b61809bfa4a6e3e1b1d76b282bf902b8e0710a
Sudarshan Shubakar <sudarshanshubakar@gmail.com>
2026-08-27 19:50:07 +0530

    made the loading code deterministic

--- loadgen/stream-lab-producer/src/main/java/TradesProducer.java
+++ loadgen/stream-lab-producer/src/main/java/TradesProducer.java
@@ -5,7 +5,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.jspecify.annotations.NonNull;
 import org.yaml.snakeyaml.Yaml;
 
@@ -15,7 +15,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
 
 public class TradesProducer {
     public static void main(String[] args) {
@@ -25,20 +24,20 @@ public class TradesProducer {
         String[] sides = new String[]{"BUY", "SELL"};
         int countMessages = (int) labProps.get("totalMessages");
         int recordsRate = (int) labProps.get("recordsRate");
-        long sleep = 1000/recordsRate;
         int latePct = (int) labProps.get("latePercent");
         int maxLateNess = (int) labProps.get("maxLatenessMs");
         String topic = labProps.get("topicName").toString();
 
-        int lateCnt = countMessages * latePct/100;
-        System.out.println(lateCnt);
-        try (Producer<Integer, Trade> kafkaProducer = new KafkaProducer<>(props)) {
+        int randomSeed = (int) labProps.get("randomSeed");
+        Random seededRand = new Random(randomSeed);
+
+        try (Producer<String, Trade> kafkaProducer = new KafkaProducer<>(props)) {
             for (int i = 0; i < countMessages; i++) {
-                String sym = symbols[countMessages % symbols.length];
-                double price = getPrice(1.0, 1000.0, 3);
-                int qty = ThreadLocalRandom.current().nextInt(1, 1000);
-                String side = sides[Math.toIntExact(System.currentTimeMillis() % sides.length)];
-                long eventTime = getEventTime(i, lateCnt, maxLateNess);
+                String sym = symbols[i % symbols.length];
+                double price = getPrice(1.0, 1000.0, 3, seededRand);
+                int qty = seededRand.nextInt(1, 1000);
+                String side = sides[seededRand.nextInt(sides.length)];
+                long eventTime = getEventTime(i, latePct, maxLateNess, seededRand);
                 Trade trade = Trade.newBuilder()
                         .setSymbol(sym)
                         .setPrice(price)
@@ -46,20 +45,16 @@ public class TradesProducer {
                         .setSide(side)
                         .setEventTime(eventTime)
                         .build();
-                kafkaProducer.send(new ProducerRecord<>(topic, 0, System.currentTimeMillis(), 1, trade));
-                Thread.sleep(sleep);
+                kafkaProducer.send(new ProducerRecord<>(topic, trade.getSymbol().toString(), trade));
 
             }
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
         }
     }
 
-    private static long getEventTime(int msgNumber, int lateCnt, long maxLateNess) {
+    private static long getEventTime(int msgNumber, int latePct, long maxLateNess, Random seededRandom) {
         long now = System.currentTimeMillis();
-        if(msgNumber % lateCnt == 0) {
-            now = now - ThreadLocalRandom.current().nextLong(0, maxLateNess);
-            System.out.println(msgNumber +" --> LATE");
+        if(seededRandom.nextInt(100) < latePct) {
+            now = now - seededRandom.nextLong(0, maxLateNess);
         }
         return now;
     }
@@ -75,7 +70,6 @@ public class TradesProducer {
                 .getClassLoader()
                 .getResourceAsStream("lab.yaml")) {
             yMap = yaml.load(inputStream);
-//            System.out.println(yMap);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -85,16 +79,16 @@ public class TradesProducer {
     private static @NonNull Properties getProperties() {
         Properties props = new Properties();
         props.put("bootstrap.servers", "localhost:9092");
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class.getName());
         return props;
     }
 
-    private static double getPrice(double low, double high, int decimalPlaces) {
+    private static double getPrice(double low, double high, int decimalPlaces, Random seededRandom) {
         int base = 10;
         double d = Math.pow(base, decimalPlaces);
-        double rand = ThreadLocalRandom.current().nextDouble(low, high);
+        double rand = seededRandom.nextDouble(low, high);
         return Math.round(rand * d) / d;
     }
 }
